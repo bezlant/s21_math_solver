1edc30b117b01c7d59a4349aa1019455a652b737
Anthony <89563512+bezlant@users.noreply.github.com>
2022-07-25 18:12:37 +0300

    fixes(stack): now operates with long doubles (#5)
    * Stack works with ldouble now
    * Fixed Makefile to compile on linux

--- src/stack/stack.h
+++ src/stack/stack.h
@@ -1,7 +1,6 @@
 #ifndef STACK_H_
 #define STACK_H_
 
-#include "../calc.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,15 +8,15 @@
 #define STACK_SIZE 1024
 
 struct my_stack {
-    int stack[STACK_SIZE];
+    long double stack[STACK_SIZE];
     int idx;
 };
 
 struct my_stack *init_stack();
-void push(struct my_stack *const s, const int x);
-int pop(struct my_stack *s);
+void push(struct my_stack *const s, long double x);
+long double pop(struct my_stack *s);
 bool is_empty(struct my_stack *s);
-int peek(struct my_stack *s);
+long double peek(struct my_stack *s);
 void my_stack_free(struct my_stack *s);
 
 #endif
